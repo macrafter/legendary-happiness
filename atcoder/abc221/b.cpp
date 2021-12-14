aa9d7c433c8cb7359e3ac0f463ac1cedb1317021
Madhav Krishna <macrafterburger@gmail.com>
2021-12-14 13:07:49 +0530

    khkj

--- atcoder/abc221/b.cpp
+++ atcoder/abc221/b.cpp
@@ -8,12 +8,12 @@ int main() {
 
 	if(s==t){
 		cout << "Yes";
+		return 0;
 	}else{
 		for(int i=0;i<s.length()-1;i++){
 			a[i]=t[i+1];
 			a[i+1]=t[i];
 
-			cout << a << endl;
 			if(a==s){
 				cout << "Yes";
 				return 0;
